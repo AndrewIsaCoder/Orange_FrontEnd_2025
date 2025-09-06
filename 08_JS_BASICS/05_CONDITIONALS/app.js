02d0415cc154b6c3ec640f91c5658f356186d5ba
andrew <93436527+AndrewIsaCoder@users.noreply.github.com>
2025-09-06 07:38:18 +0000

    Add initial HTML, CSS, and JavaScript files for conditionals implementation

---
+++ 08_JS_BASICS/05_CONDITIONALS/app.js
@@ -0,0 +1,17 @@
+if(conditie)  {
+    // cod care se executa daca conditia este adevarata
+}
+
+if(conditie) {
+
+} else {
+    // cod care se executa daca conditia este falsa
+}
+
+if (conditie1) {
+    // cod care se executa daca conditia1 este adevarata
+} else if (conditie2) {
+    // cod care se executa daca conditia2 este adevarata
+} else if(conditie3) {
+    // cod care se executa daca conditia3 este adevarata
+}
