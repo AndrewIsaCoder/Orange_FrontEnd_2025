985de19800329a8e71dcd10241642812aae346fc
andrew <93436527+AndrewIsaCoder@users.noreply.github.com>
2025-09-06 07:47:29 +0000

    Refactor app.js to comment out conditional examples; update index.html title and structure for tax calculation; add initial CSS styles for layout and design.

--- 08_JS_BASICS/05_CONDITIONALS/app.js
+++ 08_JS_BASICS/05_CONDITIONALS/app.js
@@ -1,17 +1,19 @@
-if(conditie)  {
-    // cod care se executa daca conditia este adevarata
-}
+// if(conditie)  {
+//     // cod care se executa daca conditia este adevarata
+// }
 
-if(conditie) {
+// if(conditie) {
 
-} else {
-    // cod care se executa daca conditia este falsa
-}
+// } else {
+//     // cod care se executa daca conditia este falsa
+// }
 
-if (conditie1) {
-    // cod care se executa daca conditia1 este adevarata
-} else if (conditie2) {
-    // cod care se executa daca conditia2 este adevarata
-} else if(conditie3) {
-    // cod care se executa daca conditia3 este adevarata
-}
+// if (conditie1) {
+//     // cod care se executa daca conditia1 este adevarata
+// } else if (conditie2) {
+//     // cod care se executa daca conditia2 este adevarata
+// } else if(conditie3) {
+//     // cod care se executa daca conditia3 este adevarata
+// } else if (conditie4) {
+//     // cod care se executa daca conditia4 este adevarata
+// }
