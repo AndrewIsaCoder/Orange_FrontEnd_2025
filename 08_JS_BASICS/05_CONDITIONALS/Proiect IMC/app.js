f4facb4ee6b133ebd0e4b40f4125887ad6daa6c5
Stoian Andrei <stoian.andrei239@gmail.com>
2025-09-06 12:45:03 +0300

    Add IMC calculator functionality with input fields and result display

---
+++ 08_JS_BASICS/05_CONDITIONALS/Proiect IMC/app.js
@@ -0,0 +1,36 @@
+var inputGreutate = document.getElementById("greutate");
+var inputInaltime = document.getElementById("inaltime");
+var paragrafRezultat = document.getElementById("text-rezultat");
+
+console.log("dfdfddf");
+
+function calculeazaImc() {
+  var greutate = Number(inputGreutate.value);
+  var inaltime = Number(inputInaltime.value);
+  var imc = greutate / (inaltime/100) ** 2;
+
+  console.log(imc);
+
+  if (imc < 18.5) {
+    paragrafRezultat.innerHTML =
+      "IMC sub " +
+      imc +
+      " Risc pentru sănătate: ridicat Această valoare indică riscuri pentru sănătatea ta, este mult prea mică pentru o sănătate optimă.";
+  } else if (imc <= 18.5 && imc > 24.9) {
+    paragrafRezultat.innerHTML =
+      "IMC " + imc + " Risc pentru sănătate: minim/scăzut";
+  } else if (imc <= 25 && imc > 29.9) {
+    paragrafRezultat.innerHTML =
+      "IMC " + imc + " Risc pentru sănătate: scăzut/moderat";
+  } else if (imc <= 30 && imc > 34.9) {
+    paragrafRezultat.innerHTML =
+      "IMC " +
+      imc +
+      " Risc pentru sănătate: moderat/ridicat Acesta e un semn de avertisment: schimbă-ți dieta acum!";
+  } else if (imc > 35) {
+    paragrafRezultat.innerHTML =
+      "IMC " +
+      imc +
+       " Risc pentru sănătate: ridicat Greutatea iți afectează în mod radical sănătatea. Scapă de surplusul de greutate!";
+  }
+}
