0cb4c6bb4982f10d4baea240a0181af09af0b9b4
Stoian Andrei <stoian.andrei239@gmail.com>
2025-09-06 12:47:53 +0300

    Refactor IMC calculation logic to display formatted IMC values; update CSS for improved styling and responsiveness.

--- 08_JS_BASICS/05_CONDITIONALS/Proiect IMC/app.js
+++ 08_JS_BASICS/05_CONDITIONALS/Proiect IMC/app.js
@@ -8,29 +8,25 @@ function calculeazaImc() {
   var greutate = Number(inputGreutate.value);
   var inaltime = Number(inputInaltime.value);
   var imc = greutate / (inaltime/100) ** 2;
-
-  console.log(imc);
+  var imcAfisat = imc.toFixed(2);
 
   if (imc < 18.5) {
     paragrafRezultat.innerHTML =
-      "IMC sub " +
-      imc +
-      " Risc pentru sănătate: ridicat Această valoare indică riscuri pentru sănătatea ta, este mult prea mică pentru o sănătate optimă.";
-  } else if (imc <= 18.5 && imc > 24.9) {
+      "IMC sub " + imcAfisat +
+      ". Risc pentru sănătate: ridicat. Această valoare indică riscuri pentru sănătatea ta, este mult prea mică pentru o sănătate optimă.";
+  } else if (imc >= 18.5 && imc <= 24.9) {
     paragrafRezultat.innerHTML =
-      "IMC " + imc + " Risc pentru sănătate: minim/scăzut";
-  } else if (imc <= 25 && imc > 29.9) {
+      "IMC " + imcAfisat + ". Risc pentru sănătate: minim/scăzut.";
+  } else if (imc >= 25 && imc <= 29.9) {
     paragrafRezultat.innerHTML =
-      "IMC " + imc + " Risc pentru sănătate: scăzut/moderat";
-  } else if (imc <= 30 && imc > 34.9) {
+      "IMC " + imcAfisat + ". Risc pentru sănătate: scăzut/moderat.";
+  } else if (imc >= 30 && imc <= 34.9) {
     paragrafRezultat.innerHTML =
-      "IMC " +
-      imc +
-      " Risc pentru sănătate: moderat/ridicat Acesta e un semn de avertisment: schimbă-ți dieta acum!";
-  } else if (imc > 35) {
+      "IMC " + imcAfisat +
+      ". Risc pentru sănătate: moderat/ridicat. Acesta e un semn de avertisment: schimbă-ți dieta acum!";
+  } else if (imc >= 35) {
     paragrafRezultat.innerHTML =
-      "IMC " +
-      imc +
-       " Risc pentru sănătate: ridicat Greutatea iți afectează în mod radical sănătatea. Scapă de surplusul de greutate!";
+      "IMC " + imcAfisat +
+      ". Risc pentru sănătate: ridicat. Greutatea iți afectează în mod radical sănătatea. Scapă de surplusul de greutate!";
   }
 }
