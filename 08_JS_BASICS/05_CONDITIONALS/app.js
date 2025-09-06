c2db63d406335e0163add74932868a8724f4c129
Stoian Andrei <stoian.andrei239@gmail.com>
2025-09-06 11:35:26 +0300

    Implement auto tax calculation logic and update input types for better validation

--- 08_JS_BASICS/05_CONDITIONALS/app.js
+++ 08_JS_BASICS/05_CONDITIONALS/app.js
@@ -19,7 +19,23 @@
 // }
 
 var inputCapacitateCilindrica = document.getElementById("capacitate-cilindrica");
+var paragradRezultat = document.getElementById("rezultat");
 
-function calculeazImpozitAuto() {
+function calculeazaImpozitAuto() {
+    var impozitAuto = 0;
+    var capacitateCilindrica = Number(inputCapacitateCilindrica.value);
 
+    if (capacitateCilindrica <= 1600) {
+        impozitAuto = (capacitateCilindrica / 200) * 10;
+    } else if (capacitateCilindrica > 1600 && capacitateCilindrica <= 2000) {
+        impozitAuto = (capacitateCilindrica / 200) * 28;
+    } else if (capacitateCilindrica > 2000 && capacitateCilindrica <= 2600) {
+        impozitAuto = (capacitateCilindrica / 200) * 107;
+    } else if (capacitateCilindrica > 2600 && capacitateCilindrica <= 3000) {
+        impozitAuto = (capacitateCilindrica / 200) * 215;
+    } else if (capacitateCilindrica > 3000) {
+        impozitAuto = (capacitateCilindrica / 200) * 434;
+    }
+
+    paragradRezultat.innerHTML = "Aveti de platit " + impozitAuto + "lei";
 }
